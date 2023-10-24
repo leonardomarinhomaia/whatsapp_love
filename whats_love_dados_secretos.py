941c2c8cffdf3cbfd1dcb9be170ea861ec8a6431
Leonardo Marinho Maia <112647015+leonardomarinhomaia@users.noreply.github.com>
2023-10-24 18:37:22 -0300

    Create whats_love_dados_secretos.py

---
+++ whats_love_dados_secretos.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+mensagens = ['Mensagem 1', 'Mensagem 2', 'Mensagem 3', 'Mensagem 4', 'Mensagem 5', 'Mensagem 6', 'Mensagem 7', 'Mensagem 8', 'Mensagem 9', 'Mensagem 10']
+
